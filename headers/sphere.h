487f8ad755e8b3515e129b06dae878294d8b9c93
hriztam <hritamstark05@gmail.com>
2026-01-07 02:36:16 +0530

    Modeling Light Scatter and Reflectance

--- headers/sphere.h
+++ headers/sphere.h
@@ -6,7 +6,10 @@
 class sphere : public hittable
 {
 public:
-    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius))
+    {
+        // TODO: Initialize the material pointer `mat`.
+    }
 
     bool hit(const ray &r, interval ray_t, hit_record &rec) const override
     {
@@ -34,6 +37,7 @@ public:
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -41,6 +45,7 @@ public:
 private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
 
 #endif
